9ee4a9b7bc7ca90368814c994afdafe13272f1cf
fluwer <flashkoshnik@gmail.com>
2022-10-19 17:07:57 +0300

    lab2 with error

---
+++ Laboratory work 2/node.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_set>
+#include <list>
+#include <SFML\Graphics.hpp>
+
+#include <queue>
+
+#ifndef NODE_H
+#define NODE_H
+
+using namespace std;
+
+struct Node
+{
+public:
+    Node(int local_depth);
+    char** elements;
+    int depth;
+    struct Node* left;
+    struct Node* up;
+    struct Node* down;
+    struct Node* right;
+    struct Node* parent;
+    static void finalPath(Node*);
+
+    int priority;
+};
+auto comparator = [](const Node* l, const Node* r)->bool {return l->priority > r->priority; };
+
+#endif
